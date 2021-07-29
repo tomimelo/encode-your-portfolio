60610bf6b96141c89db5f94f1ccc2f4657061f5e
Tomas Melone <tomasmelone@hotmail.com>
2021-07-29 22:49:53 +0200

    style: sidebar menu

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
+  public panelHidden: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
