1c2252cd1cc9fcd2d028a0f359c035172fc447f8
Tomas Melone <tomasmelone@hotmail.com>
2021-08-12 20:44:33 +0200

    feat: My projects page and small style improvements

--- src/app/components/my-projects/my-projects.component.ts
+++ src/app/components/my-projects/my-projects.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyProjectsComponent implements OnInit {
 
+  public projects: any[] = [
+    {
+      title: "Project A",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat lorem sed dui eleifend luctus...",
+      img: "../../../assets/imgs/placeholder-image.png"
+    },
+    {
+      title: "Project B",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat lorem sed dui eleifend luctus...",
+      img: "../../../assets/imgs/placeholder-image.png"
+    },
+    {
+      title: "Project C",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat lorem sed dui eleifend luctus...",
+      img: "../../../assets/imgs/placeholder-image.png"
+    },
+    {
+      title: "Project D",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec consequat lorem sed dui eleifend luctus...",
+      img: "../../../assets/imgs/placeholder-image.png"
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
